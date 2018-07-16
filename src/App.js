4a9b06f79fcac7f53c9bc15ac4eb031004ae5c80
Nikita Zamiatin <4406015@gmail.com>
2018-07-16 13:57:33 -0400

    Routes:Home &  Not Found

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import NavBar from './NavBar'
 import Button from 'react-toolbox/lib/button/Button';
+import {Switch , Route} from "react-router-dom"
+import Home from "./Home"
 
-// import {Switch , Route} from "react-router-dom"
 
+
+const NotFound = () => <h1>Not Found</h1>
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar />
-        <header className="App-header">
-          <h1 className="App-title">Welcome to AnteUp</h1>
-
-           <Button label="Hello World!" />
-        </header>
+        <Switch>
+           <Route exact path='/' component={Home} />
+            <Route  path='*' component={NotFound} />
+        </Switch>
       </div>
     );
   }
--- src/NavBar.js
+++ src/NavBar.js
@@ -29,20 +29,12 @@ class NavBar extends Component {
         defaultSelectedKeys={['2']}
         style={{ lineHeight: '64px' }}
       >
-
-        <Menu.Item key="2">nav 2</Menu.Item>
+        <Menu.Item key="2">AnteUp</Menu.Item>
            <Avatar src="./Media/image.png" />
         <Menu.Item key="3">nav 3</Menu.Item>
       </Menu>
     </Header>
-    <Content style={{ padding: '0 50px' }}>
-      <Breadcrumb style={{ margin: '16px 0' }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
-      </Breadcrumb>
-      <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
-    </Content>
+
     <Footer style={{ textAlign: 'center' }}>
       Ant Design ©2016 Created by Ant UED
     </Footer>
