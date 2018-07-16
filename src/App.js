b2279d9af240b191ebfe25fad839b79ff8186466
Nikita Zamiatin <4406015@gmail.com>
2018-07-16 13:30:10 -0400

    install antd logo and layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './NavBar'
+import Button from 'react-toolbox/lib/button/Button';
+
 // import {Switch , Route} from "react-router-dom"
 
 
@@ -8,10 +10,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <header className="App-header">
           <h1 className="App-title">Welcome to AnteUp</h1>
+
+           <Button label="Hello World!" />
         </header>
-        <NavBar />
       </div>
     );
   }
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
+import './App.css';
+import {
+    Layout,
+    Menu,
+    Icon,
+    SubMenu,
+    Breadcrumb,
+    Avatar,
+
+} from 'antd';
+
+const {
+    Header,
+    Content,
+    Footer
+} = Layout;
 
 
 class NavBar extends Component {
-
-    render (){
-        return (
-            <div> Hello I'm first your Component</div>
-        )
+  render() {
+    return (
+      <Layout className="layout">
+    <Header>
+      <div className="logo" />
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['2']}
+        style={{ lineHeight: '64px' }}
+      >
+        <Menu.Item key="1"><Avatar scr="https://snag.gy/na0lB3.jpg"/></Menu.Item>
+        <Menu.Item key="2">nav 2</Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
+      </Menu>
+    </Header>
+    <Content style={{ padding: '0 50px' }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>List</Breadcrumb.Item>
+        <Breadcrumb.Item>App</Breadcrumb.Item>
+      </Breadcrumb>
+      <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>
+      Ant Design ©2016 Created by Ant UED
+    </Footer>
+  </Layout>
+    );
     }
 }
 
-export default NavBar
+export default NavBar;
